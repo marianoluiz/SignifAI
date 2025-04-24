e14d8d09f49ff5baae57c266f2101622338361af
marianoluiz <kmarianu@gmail.com>
2025-04-25 03:35:11 +0800

    added audio and image assets

---
+++ src/constants/audio.ts
@@ -0,0 +1,10 @@
+import click_sound from "../assets/audio/click_sound.mp3"
+import wonderful_world from "../assets/audio/wonderful_world.mp3"
+
+export const AUDIO = {
+    /* UI sound */
+    click_sound,
+
+    /* ON-GAME sounds */
+    wonderful_world
+}
--- src/constants/images.ts
+++ src/constants/images.ts
@@ -1,14 +1,91 @@
+/* UI assets */
+import bg_glitch1 from "/src/assets/img/start_screen/bg_glitch1.png";
+import bg_glitch2 from "/src/assets/img/start_screen/bg_glitch2.png";
+import bg_glitch3 from "/src/assets/img/start_screen/bg_glitch3.png";
+import btn_lboard from "/src/assets/img/start_screen/btn_lboard.png";
+import btn_play from "/src/assets/img/start_screen/btn_play.png";
+import btn_pract from "/src/assets/img/start_screen/btn_pract.png";
+import main_img from "/src/assets/img/start_screen/main_img.png";
+import setting_icon from "/src/assets/img/start_screen/setting.png";
+import sound_icon from "/src/assets/img/start_screen/sound.png";
+import select_scr_bg from "/src/assets/img/select_song/select_scr_bg.jpg";
+import choice_frame1 from "/src/assets/img/select_song/choice_frame1.png"
+import choice_frame2 from "/src/assets/img/select_song/choice_frame2.png"
+import back_btn from "/src/assets/img/select_song/back_btn.png"
+
+/* ON-GAME assets */
+import a from "/src/assets/img/sign/A_test.jpg";
+import b from "/src/assets/img/sign/B_test.jpg";
+import c from "/src/assets/img/sign/C_test.jpg";
+import d from "/src/assets/img/sign/D_test.jpg";
+import e from "/src/assets/img/sign/E_test.jpg";
+import f from "/src/assets/img/sign/F_test.jpg";
+import g from "/src/assets/img/sign/G_test.jpg";
+import h from "/src/assets/img/sign/H_test.jpg";
+import i from "/src/assets/img/sign/I_test.jpg";
+import j from "/src/assets/img/sign/J_test.jpg";
+import k from "/src/assets/img/sign/K_test.jpg";
+import l from "/src/assets/img/sign/L_test.jpg";
+import m from "/src/assets/img/sign/M_test.jpg";
+import n from "/src/assets/img/sign/N_test.jpg";
+import o from "/src/assets/img/sign/O_test.jpg";
+import p from "/src/assets/img/sign/P_test.jpg";
+import q from "/src/assets/img/sign/Q_test.jpg";
+import r from "/src/assets/img/sign/R_test.jpg";
+import s from "/src/assets/img/sign/S_test.jpg";
+import t from "/src/assets/img/sign/T_test.jpg";
+import u from "/src/assets/img/sign/U_test.jpg";
+import v from "/src/assets/img/sign/V_test.jpg";
+import w from "/src/assets/img/sign/W_test.jpg";
+import x from "/src/assets/img/sign/X_test.jpg";
+import y from "/src/assets/img/sign/Y_test.jpg";
+import z from "/src/assets/img/sign/Z_test.jpg";
+import thank_you from "/src/assets/img/sign/TY_test.jpg";
+
 export const IMAGES = {
     /* start_screen */
-    bg_glitch1: "/src/assets/img/start_screen/bg_glitch1.png",
-    bg_glitch2: "/src/assets/img/start_screen/bg_glitch2.png",
-    bg_glitch3: "/src/assets/img/start_screen/bg_glitch3.png",
-    btn_lboard: "/src/assets/img/start_screen/btn_lboard.png",
-    btn_play: "/src/assets/img/start_screen/btn_play.png",
-    btn_pract: "/src/assets/img/start_screen/btn_pract.png",
-    main_img: "/src/assets/img/start_screen/main_img.png",
-    setting_icon: "/src/assets/img/start_screen/setting.png",
-    sound_icon: "/src/assets/img/start_screen/sound.png",
+    bg_glitch1,
+    bg_glitch2,
+    bg_glitch3,
+    btn_lboard,
+    btn_play,
+    btn_pract,
+    main_img,
+    setting_icon,
+    sound_icon,
 
-    /*  */
-}
+    /* select_screen */
+    select_scr_bg,
+    choice_frame1,
+    choice_frame2,
+    back_btn,
+
+    /* sign */
+    a,
+    b,
+    c,
+    d,
+    e,
+    f,
+    g,
+    h,
+    i,
+    j,
+    k,
+    l,
+    m,
+    n,
+    o,
+    p,
+    q,
+    r,
+    s,
+    t,
+    u,
+    v,
+    w,
+    x,
+    y,
+    z,
+    thank_you
+};
