4db4d1b19b027e8dd35646f0ac96e730206d74eb
marianoluiz <kmarianu@gmail.com>
2025-04-24 20:27:27 +0800

    added image constants

---
+++ src/constants/images.ts
@@ -0,0 +1,14 @@
+export const IMAGES = {
+    /* start_screen */
+    bg_glitch1: "/src/assets/img/start_screen/bg_glitch1.png",
+    bg_glitch2: "/src/assets/img/start_screen/bg_glitch2.png",
+    bg_glitch3: "/src/assets/img/start_screen/bg_glitch3.png",
+    btn_lboard: "/src/assets/img/start_screen/btn_lboard.png",
+    btn_play: "/src/assets/img/start_screen/btn_play.png",
+    btn_pract: "/src/assets/img/start_screen/btn_pract.png",
+    main_img: "/src/assets/img/start_screen/main_img.png",
+    setting_icon: "/src/assets/img/start_screen/setting.png",
+    sound_icon: "/src/assets/img/start_screen/sound.png",
+
+    /*  */
+}
