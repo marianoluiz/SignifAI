e422a1cbea55e0de17ce8c49bdb5bce563569df7
marianoluiz <kmarianu@gmail.com>
2025-04-25 23:16:46 +0800

    made initial song selection page

--- src/app/routes/play/game.tsx
+++ src/app/routes/play/game.tsx
@@ -1,17 +1,9 @@
-import { NavLink } from "react-router"
 
 const GamePage = () => {
   return (
     <div>
       <div>Game</div>
 
-
-      <NavLink
-        to="/play/select"
-        className="m-8 px-4 py-2 bg-violet-500 text-white rounded-md"
-      >
-        Back to song select
-      </NavLink>
     </div>
   );
 };
