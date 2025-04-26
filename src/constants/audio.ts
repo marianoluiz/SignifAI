3830300ac848159de23f342787bb58aafb85185e
marianoluiz <kmarianu@gmail.com>
2025-04-26 19:22:49 +0800

    added assets

--- src/constants/audio.ts
+++ src/constants/audio.ts
@@ -1,10 +1,16 @@
 import click_sound from "../assets/audio/click_sound.mp3"
 import wonderful_world from "../assets/audio/wonderful_world.mp3"
+import count_on_me from "../assets/audio/count_on_me.mp3"
+import you_belong_w_me from "../assets/audio/youbelongwme.mp3"
+
 
 export const AUDIO = {
     /* UI sound */
     click_sound,
 
-    /* ON-GAME sounds */
-    wonderful_world
+    /* ON-GAME music */
+    wonderful_world,
+    count_on_me,
+    you_belong_w_me
+
 }
