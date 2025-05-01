1a570fe44dd6327cf00d4a173511b0c5e400d4f8
marianoluiz <kmarianu@gmail.com>
2025-05-02 02:36:19 +0800

    modified and added assets

--- src/constants/images.ts
+++ src/constants/images.ts
@@ -9,8 +9,7 @@ import main_img from "/src/assets/img/start_screen/main_img.png";
 import setting_icon from "/src/assets/img/start_screen/setting.png";
 import sound_icon from "/src/assets/img/start_screen/sound.png";
 import select_scr_bg from "/src/assets/img/select_song/select_scr_bg.jpg";
-// import choice_frame1 from "/src/assets/img/select_song/choice_frame1.png"
-// import choice_frame2 from "/src/assets/img/select_song/choice_frame2.png"
+
 import wonderful_world_album from "/src/assets/img/select_song/wonder_world-album.png"
 import count_on_me_album from "/src/assets/img/select_song/count_on_me-album.png"
 import you_belong_w_me_album from "/src/assets/img/select_song/you_belong_w_me-album.png"
@@ -28,24 +27,39 @@ import results_bg from "/src/assets/img/results/results_bg.webp"
 import result_hand_design from "/src/assets/img/results/result_hand_design.png"
 import result_stars from "/src/assets/img/results/result_stars.png"
 import score_outline from "/src/assets/img/results/score_outline.png"
-import disco_img from "/src/assets/img/results/disco_img.png"
+import disco_img from "/src/assets/img/results/disco_img.webp"
  
 
 /* signs */
+import A from "/src/assets/img/signs/A.png";
 import B from "/src/assets/img/signs/B.png";
 import C from "/src/assets/img/signs/C.png";
 import D from "/src/assets/img/signs/D.png";
+import E from "/src/assets/img/signs/E.png";
 import F from "/src/assets/img/signs/F.png";
-import I_love_you from "/src/assets/img/signs/ILY.png";
+import G from "/src/assets/img/signs/G.png";
+import H from "/src/assets/img/signs/H.png";
+import I from "/src/assets/img/signs/I.png";
+// Skipping J
+import K from "/src/assets/img/signs/K.png";
 import L from "/src/assets/img/signs/L.png";
-import Me from "/src/assets/img/signs/ME.png";
+import M from "/src/assets/img/signs/M.png";
+import N from "/src/assets/img/signs/N.png";
+import O from "/src/assets/img/signs/O.png";
+import P from "/src/assets/img/signs/P.png";
+import Q from "/src/assets/img/signs/Q.png";
 import R from "/src/assets/img/signs/R.png";
 import S from "/src/assets/img/signs/S.png";
 import T from "/src/assets/img/signs/T.png";
+import U from "/src/assets/img/signs/U.png";
 import V from "/src/assets/img/signs/V.png";
 import W from "/src/assets/img/signs/W.png";
+import X from "/src/assets/img/signs/X.png";
+// Skipping Z
 import Y from "/src/assets/img/signs/Y.png";
 import You from "/src/assets/img/signs/YOU.png";
+import I_love_you from "/src/assets/img/signs/ILY.png";
+import Me from "/src/assets/img/signs/ME.png";
 
 export const IMAGES = {
     /* start_screen */
@@ -87,18 +101,31 @@ export const IMAGES = {
     disco_img,
 
     /* signs */
+    A,
     B, 
     C, 
     D, 
+    E,
     F, 
-    I_love_you, 
+    G,
+    H,
+    I,
+    K,
     L, 
-    Me, 
+    M,
+    N,
+    O,
+    P,
+    Q,
     R, 
     S, 
     T, 
+    U,
     V, 
     W, 
+    X,
     Y, 
-    You
+    You,
+    Me, 
+    I_love_you, 
 };
