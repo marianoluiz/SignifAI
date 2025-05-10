e526137aaa485555c26dbf36a05ff84e1f675997
marianoluiz <kmarianu@gmail.com>
2025-05-10 19:55:28 +0800

    modified asset

--- src/constants/images.ts
+++ src/constants/images.ts
@@ -1,4 +1,4 @@
-/* UI assets */
+/* Home assets */
 import bg_glitch1 from "/src/assets/img/start_screen/bg_glitch1.png";
 import bg_glitch2 from "/src/assets/img/start_screen/bg_glitch2.png";
 import bg_glitch3 from "/src/assets/img/start_screen/bg_glitch3.png";
@@ -8,14 +8,17 @@ import btn_pract from "/src/assets/img/start_screen/btn_pract.png";
 import main_img from "/src/assets/img/start_screen/main_img.png";
 import setting_icon from "/src/assets/img/start_screen/setting.png";
 import sound_icon from "/src/assets/img/start_screen/sound.png";
-import select_scr_bg from "/src/assets/img/select_song/select_scr_bg.jpg";
 
+/* Song Selection Assets */
+import select_scr_bg from "/src/assets/img/select_song/select_scr_bg.jpg";
 import wonderful_world_album from "/src/assets/img/select_song/wonder_world-album.png"
 import count_on_me_album from "/src/assets/img/select_song/count_on_me-album.png"
 import you_belong_w_me_album from "/src/assets/img/select_song/you_belong_w_me-album.png"
 import wonderful_world_palbum from "/src/assets/img/select_song/wonderful_world-palbum.png"
 import count_on_me_palbum from "/src/assets/img/select_song/count_on_me-palbum.png"
 import you_belong_w_me_palbum from "/src/assets/img/select_song/you_belong_w_me-palbum.png"
+
+/* On Game Assets */
 import count_on_me_bg from "/src/assets/img/on_game/count_on_me-bg.webp"
 import wonderful_world_bg from "/src/assets/img/on_game/wonderful_world-bg.webp"
 import you_belong_w_me_bg from "/src/assets/img/on_game/you_belong_w_me-bg.webp"
@@ -23,10 +26,14 @@ import MISS_rating_effect from "/src/assets/img/on_game/miss_rating_effect.png"
 import OK_rating_effect from "/src/assets/img/on_game/ok_rating_effect.png"
 import GOOD_rating_effect from "/src/assets/img/on_game/good_rating_effect.png"
 import PERFECT_rating_effect from "/src/assets/img/on_game/perfect_rating_effect.png"
+
+/* Result Page Assets */
 import results_bg from "/src/assets/img/results/results_bg.webp"
 import result_hand_design from "/src/assets/img/results/result_hand_design.png"
 import result_stars from "/src/assets/img/results/result_stars.png"
 import score_outline from "/src/assets/img/results/score_outline.png"
+
+/* Leaderboard Page Asset */
 import disco_img from "/src/assets/img/results/disco_img.webp"
  
 
@@ -40,7 +47,7 @@ import F from "/src/assets/img/signs/F.png";
 import G from "/src/assets/img/signs/G.png";
 import H from "/src/assets/img/signs/H.png";
 import I from "/src/assets/img/signs/I.png";
-// Skipping J
+    // Skipping J
 import K from "/src/assets/img/signs/K.png";
 import L from "/src/assets/img/signs/L.png";
 import M from "/src/assets/img/signs/M.png";
@@ -55,7 +62,7 @@ import U from "/src/assets/img/signs/U.png";
 import V from "/src/assets/img/signs/V.png";
 import W from "/src/assets/img/signs/W.png";
 import X from "/src/assets/img/signs/X.png";
-// Skipping Z
+    // Skipping Z
 import Y from "/src/assets/img/signs/Y.png";
 import You from "/src/assets/img/signs/YOU.png";
 import I_love_you from "/src/assets/img/signs/ILY.png";
@@ -90,17 +97,7 @@ export const IMAGES = {
     OK_rating_effect,
     GOOD_rating_effect,
     PERFECT_rating_effect,
-    
-    /* results */
-    results_bg,
-    result_hand_design,
-    result_stars,
-    score_outline,
-    
-    /* scores */
-    disco_img,
-
-    /* signs */
+        // Hand Signs
     A,
     B, 
     C, 
@@ -127,5 +124,14 @@ export const IMAGES = {
     Y, 
     You,
     Me, 
-    I_love_you, 
+    I_love_you,
+
+    /* results page*/
+    results_bg,
+    result_hand_design,
+    result_stars,
+    score_outline,
+    
+    /* leaderboard page  */
+    disco_img,
 };
